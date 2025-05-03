aaa1c95c861ff2eccae825c0336d6b56d4bbcda0
Skanda Nagendra <shriskanda@outlook.com>
2025-05-03 15:27:50 -0400

    aSDSFDDF

--- main.c
+++ main.c
@@ -17,7 +17,7 @@ int main()
     if (csv_file == NULL)
     {
         perror("Error opening the CSV file");
-        return 16;
+        return 19;
     }
     // Parse CSV data and create users
     user_t *users = read_CSV_and_create_users(csv_file, 50);
